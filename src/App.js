e065d0b6f573647582c71d3dfc05af845a9e574b
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2023-09-20 22:59:49 +0500

    flexbox-resonsive-cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import users from "./data";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="fcontainer">
+      <div className="fitem item1">1</div>
+      <div className="fitem item2">2</div>
+      <div className="fitem item3">3</div>
+      <div className="fitem item4">4</div>
+     </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,123 @@
+const users = [
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "John Doe",
+    designation: "Marketing Manager",
+    social_media_icons: {
+      facebook: "https://facebook.com/johndoe",
+      twitter: "https://twitter.com/johndoe",
+      instagram: "https://instagram.com/johndoe",
+      linkedin: "https://linkedin.com/in/johndoe",
+    },
+    number_of_posts: 150,
+    number_of_followers: 5000,
+    number_of_followings: 300,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "Alice Smith",
+    designation: "Software Engineer",
+    social_media_icons: {
+      facebook: "https://facebook.com/alicesmith",
+      twitter: "https://twitter.com/alicesmith",
+      instagram: "https://instagram.com/alicesmith",
+      linkedin: "https://linkedin.com/in/alicesmith",
+    },
+    number_of_posts: 300,
+    number_of_followers: 10000,
+    number_of_followings: 400,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "Emily Johnson",
+    designation: "Graphic Designer",
+    social_media_icons: {
+      facebook: "https://facebook.com/emilyjohnson",
+      twitter: "https://twitter.com/emilyjohnson",
+      instagram: "https://instagram.com/emilyjohnson",
+      linkedin: "https://linkedin.com/in/emilyjohnson",
+    },
+    number_of_posts: 75,
+    number_of_followers: 2500,
+    number_of_followings: 200,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "David Brown",
+    designation: "Teacher",
+    social_media_icons: {
+      facebook: "https://facebook.com/davidbrown",
+      twitter: "https://twitter.com/davidbrown",
+      instagram: "https://instagram.com/davidbrown",
+      linkedin: "https://linkedin.com/in/davidbrown",
+    },
+    number_of_posts: 45,
+    number_of_followers: 1200,
+    number_of_followings: 150,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "Maria Garcia",
+    designation: "Chef",
+    social_media_icons: {
+      facebook: "https://facebook.com/mariagarcia",
+      twitter: "https://twitter.com/mariagarcia",
+      instagram: "https://instagram.com/mariagarcia",
+      linkedin: "https://linkedin.com/in/mariagarcia",
+    },
+    number_of_posts: 200,
+    number_of_followers: 8000,
+    number_of_followings: 600,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "Michael Wilson",
+    designation: "Photographer",
+    social_media_icons: {
+      facebook: "https://facebook.com/michaelwilson",
+      twitter: "https://twitter.com/michaelwilson",
+      instagram: "https://instagram.com/michaelwilson",
+      linkedin: "https://linkedin.com/in/michaelwilson",
+    },
+    number_of_posts: 120,
+    number_of_followers: 3500,
+    number_of_followings: 250,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "Sophia Lee",
+    designation: "Financial Analyst",
+    social_media_icons: {
+      facebook: "https://facebook.com/sophialee",
+      twitter: "https://twitter.com/sophialee",
+      instagram: "https://instagram.com/sophialee",
+      linkedin: "https://linkedin.com/in/sophialee",
+    },
+    number_of_posts: 90,
+    number_of_followers: 1800,
+    number_of_followings: 220,
+  },
+  {
+    user_image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+    user_name: "William Clark",
+    designation: "Architect",
+    social_media_icons: {
+      facebook: "https://facebook.com/williamclark",
+      twitter: "https://twitter.com/williamclark",
+      instagram: "https://instagram.com/williamclark",
+      linkedin: "https://linkedin.com/in/williamclark",
+    },
+    number_of_posts: 60,
+    number_of_followers: 2200,
+    number_of_followings: 180,
+  },
+];
+export default users;
